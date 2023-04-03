0306fcff652ae64e6a809ec451918fbb754d5c1c
Usman Akram <usman.akram@gmail.com>
2023-04-03 13:48:03 +0500

    ejs added

--- fa20-bcs-a/11-express/express-app.js
+++ fa20-bcs-a/11-express/express-app.js
@@ -2,6 +2,17 @@ const express = require("express");
 const server = express();
 
 server.use(express.static("public"));
+server.set("view engine", "ejs");
+
+server.get("/contact-us.html", (req, res) => {
+  res.render(`contact-us`);
+});
+server.get("/friends", (req, res) => {
+  res.render(`friends`);
+});
+server.get("/", (req, res) => {
+  res.render(`homepage`);
+});
 
 server.listen(3000, () => {
   console.log("Server Started, Visit localhost:3000");
